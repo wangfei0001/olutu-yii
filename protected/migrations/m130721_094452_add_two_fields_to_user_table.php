639736108374189e1a5743d484a97df9c0dff818
Fei Wang <wangfei001@hotmail.com>
2013-07-21 22:13:28 +1000

    add request authorization, not done

---
+++ protected/migrations/m130721_094452_add_two_fields_to_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130721_094452_add_two_fields_to_user_table extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130721_094452_add_two_fields_to_user_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
